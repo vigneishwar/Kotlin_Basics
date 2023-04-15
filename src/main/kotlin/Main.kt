3f649add85aaafdf37ae6a81ce4a276b65a9d5dd
Vigneishwar <vikki1310@gmail.com>
2023-04-15 20:23:35 +1000

    kotlin basics google

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+//fun main() {
+//    println("Use the val keyword when the value doesn't change.")
+//    println("Use the var keyword when the value can change.")
+//    println("When you define a function, you define the parameters that can be passed to it.")
+//    println("When you call a function, you pass arguments for the parameters.")
+//}
+
+
+//fun main() {
+//    var discountPercentage: Int = 0
+//    var offer: String = ""
+//    val item = "Google Chromecast"
+//    discountPercentage = 20
+//    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+//
+//    println(offer)
+//}
+
+//fun main() {
+//    val numberOfAdults = 20
+//    val numberOfKids = 30
+//    val total = numberOfAdults + numberOfKids
+//    println("The total party size is: $total")
+//}
+
+
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = "$baseSalary + $bonusAmount"
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
