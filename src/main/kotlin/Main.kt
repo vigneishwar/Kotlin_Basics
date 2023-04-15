e9b5cdfe96d65c6c9fe7b5c6b3785c4f957c8b81
Vigneishwar <vikki1310@gmail.com>
2023-04-15 21:03:09 +1000

    kotlin basics google

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.sql.RowId
+
 //fun main() {
 //    println("Use the val keyword when the value doesn't change.")
 //    println("Use the var keyword when the value can change.")
@@ -39,25 +41,53 @@
 //    println("$firstNumber + $secondNumber = $result")
 //}
 
-
-fun main() {
-    val firstNumber = 10
-    val secondNumber = 5
-    val thirdNumber = 8
-
-    val result = subtract(firstNumber, secondNumber)
-    val anotherResult = subtract(firstNumber, thirdNumber)
-
-    println("$firstNumber - $secondNumber = $result")
-    println("$firstNumber - $thirdNumber = $anotherResult")
-}
-
-// Define add() function below this line
 //
-//fun add(firstNumber: Int, secondNumber: Int): Int {
-//    return firstNumber + secondNumber
+//fun main() {
+//    val firstNumber = 10
+//    val secondNumber = 5
+//    val thirdNumber = 8
+//
+//    val result = subtract(firstNumber, secondNumber)
+//    val anotherResult = subtract(firstNumber, thirdNumber)
+//
+//    println("$firstNumber - $secondNumber = $result")
+//    println("$firstNumber - $thirdNumber = $anotherResult")
+//}
+//
+//// Define add() function below this line
+////
+////fun add(firstNumber: Int, secondNumber: Int): Int {
+////    return firstNumber + secondNumber
+////}
+//
+//fun subtract(firstNumber: Int, secondNumber: Int): Int {
+//    return firstNumber - secondNumber
 //}
 
-fun subtract(firstNumber: Int, secondNumber: Int): Int {
-    return firstNumber - secondNumber
+
+
+
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId.
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
+
+fun displayAlertMessage(operatingSystem: String = "UnknownOS", emailId: String): String {
+     return ("There's a new sign-in request on $operatingSystem for your Google Account $emailId.")
 }
